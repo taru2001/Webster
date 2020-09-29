c948c8f3f3b3c46a09ed74e0d66e8c6212a418ad
ShobhitAgarwal <shobhitagarwal151@gmail.com>
2020-09-29 18:31:27 +0530

    Sab change krdia bhailog

--- newapp/apps/home/views.py
+++ newapp/apps/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
-    return render(request,"posts.html")
+    return render(request,'home/index.html')
--- newapp/newapp/asgi.py
+++ newapp/newapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Webstars project.
+ASGI config for newapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Webstars.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newapp.settings')
 
 application = get_asgi_application()
