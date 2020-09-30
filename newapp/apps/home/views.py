aafb852521cb23ea09980611da9b4d31909641d3
ShobhitAgarwal <shobhitagarwal151@gmail.com>
2020-09-30 16:51:58 +0530

    Register view created

--- newapp/apps/home/views.py
+++ newapp/apps/home/views.py
@@ -6,6 +6,19 @@ def index(request):
     return render(request,'home/index.html')
 
 def registerUser(request):
+    if request.method=='POST':
+        fname = request.POST.post['name']
+        femail = request.POST.post['email']
+        phone = request.POST.post['mobileno']
+        passw = request.POST.post['password']
+        confpass = request.POST.post['confirmpassword']
+
+        if passw!=confpass:
+            return return HttpResponse("passwords did not matched...!!");
+
+        
+
+
     return render(request,'home/register.html')
 
 def about(request):
