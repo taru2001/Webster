e585523358db43241676b70bfe97daa0a5739f3e
taru2001 <guptaurat2001@gmail.com>
2020-09-30 16:46:32 +0530

    models and username

--- newapp/apps/home/models.py
+++ newapp/apps/home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name_id=models.AutoField
+    name=models.CharField(max_length=100)
+    username=models.CharField(max_length=100)
+    mobile=models.IntegerField()
+    email=models.EmailField(max_length=100)
+    password=models.CharField(max_length=25)
+
+    def __str__(self):
+       return self.fname + " : " + self.email
